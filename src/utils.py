2a4f972e61bbb1dd667e603231cef821fa11271c
Kundu5 <kundansirvi16@gmail.com>
2025-12-23 22:11:48 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import pandas as pd
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from catboost import CatBoostRegressor
 
 
 
@@ -26,13 +28,23 @@ def save_object(file_path, obj):
     
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report ={}
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
 
-            model.fit(X_train, y_train)
+            #Skip CatBoost in GridSearch
+            if isinstance(model, CatBoostRegressor):
+                model.fit(X_train, y_train)
+
+            else:
+                para = params.get(model_name, {})
+
+                gs = GridSearchCV(model,para,cv=3,n_jobs=3,verbose=1)
+
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
 
@@ -41,7 +53,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name]=test_model_score
 
         return report
 
